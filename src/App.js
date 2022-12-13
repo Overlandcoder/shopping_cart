895c2882ff2f067805e98dbb5a67ae3ff7157704
Overlander <anuragnandal@hotmail.com>
2022-12-12 20:15:08 -0500

    Render Nav and Shop

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './components/Home';
+import Nav from './components/Nav';
+import Shop from './components/Shop';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        {/* <Nav /> */}
+        <Nav />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
         </Routes>
       </div>
     </Router>
