1aefcd22986ce98ebd3bf1abe392f154fd892f51
Overlander <anuragnandal@hotmail.com>
2022-12-13 08:05:08 -0500

    Add basic home page content

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="home">
+      <h3>Some store</h3>
+      <Link to="/shop">
+        Shop now
+      </Link>
+    </div>
   )
 }
 
