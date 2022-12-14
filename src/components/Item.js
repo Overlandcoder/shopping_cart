7409b534f49d11ed5556d6498f0616affb5cdadb
Overlander <anuragnandal@hotmail.com>
2022-12-13 19:30:12 -0500

    Let item quantities to be incremented via button as well

--- src/components/Item.js
+++ src/components/Item.js
@@ -5,18 +5,23 @@ const Item = (props) => {
   const [info, setInfo] = useState({
     name: props.name,
     price: props.price,
-    quantity: 0,
+    quantity: 1,
     id: uniqid()
   });
 
   const handleClick = (event) => {
     event.preventDefault();
-    setInfo({...info, id: uniqid()});
+    setInfo({...info, id: uniqid(), quantity: 1});
     props.handleClick(info);
   }
 
   const handleChange = (event) => {
     setInfo({ ...info, quantity: event.target.value });
+  }
+
+  const increment = (event) => {
+    event.preventDefault();
+    setInfo({ ...info, quantity: parseInt(info.quantity) + 1 });
   }
 
   return (
@@ -25,9 +30,13 @@ const Item = (props) => {
       <h4>{props.name}</h4>
       <div>${props.price}</div>
       <div className="purchase-info">
-        <form>
+        <form className="quantity-form">
           <label htmlFor="quantity" id="quantity">Quantity</label>
-          <input onChange={handleChange} type="text" id="quantity"></input>
+          <input onChange={handleChange} type="text" id="quantity" value={info.quantity}></input>
+          <div className="increment-decrement">
+            <button onClick={increment}>+</button>
+            <button>-</button>
+          </div>
           <button type="submit" onClick={handleClick}>Add to cart</button>
         </form>
       </div>
